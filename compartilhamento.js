a418d295ef52573d49b243511883ca90ee51f672
melissapiress <melissa.pires@picpay.com>
2025-02-25 18:26:07 -0300

    Jogo finalizado 🥳

--- compartilhamento.js
+++ compartilhamento.js
@@ -1,11 +1,6 @@
-// FACEBOOK
-
-if (navigator.share !== undefined){
-    navigator.share({
-        title: "Jogo - Valores J&F",
-        text:"Teste seus conhecimentos sobre os valores J&F",
-        url:"https://jogovalores-e3h7.onrender.com"
-    })
-    .then(() => console.log("Successful share"))
-    .catch((error) => console.log("Error sharing"))
+function copiarLink() {
+    let link = "https://jogovalores-e3h7.onrender.com";
+    navigator.clipboard.writeText(link).then(() => {
+        alert("Link copiado!");
+    });
 }
