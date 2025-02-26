a2e5abc89a522e954371e71e8bda4b2e67f1c105
melpiress <melissa.foneca@germinare.org.br>
2025-02-26 08:55:24 -0300

    Alterando o texto ao copiar link

--- compartilhamento.js
+++ compartilhamento.js
@@ -1,5 +1,5 @@
 function copiarLink() {
-    let link = "https://jogovalores-e3h7.onrender.com";
+    let link = "Testei o meu conhecimento sobre os valores do grupo com esse jogo. Teste você também! https://jogovalores-e3h7.onrender.com";
     navigator.clipboard.writeText(link).then(() => {
         alert("Link copiado!");
     });
